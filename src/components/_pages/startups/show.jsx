ae5b99551ffb2ba2ca3c47bfa45bcee0c3b82203
Denis Cheung <d.s.h.cheung@gmail.com>
2017-08-30 06:49:31 +0800

    startups show page half done

--- src/components/_pages/startups/show.jsx
+++ src/components/_pages/startups/show.jsx
@@ -7,6 +7,7 @@ import {
   resetStartup
 } from '../../../actions/startups'
 
+import ImageBanner from '../../shared/image-banner'
 
 const mapStateToProps = (state) => {
   return {
@@ -32,9 +33,71 @@ export default class StartupsShow extends Component {
   }
 
   render() {
+    const { startup } = this.props
+
+    if (!startup) return null
+
     return (
-      <div id="page-startups-show" className="container">
-        Template
+      <div id="pages-startups-show" className="container">
+        <section className="row basic-info">
+          <div className="col-xs-12 col-md-6">
+            <div className="aspect-16-9">
+              <ImageBanner
+                src={`${_.get(startup, "profile.banner.original", null) || "/company-logo.jpg"}`}
+                contain
+              />
+            </div>
+          </div>
+          <div className="col-xs-12 col-md-6">
+            <h1 className="name">{startup.name}</h1>
+            <p className="overview">{startup.profile.overview}</p>
+            <div className="fundings clearfix">
+              <div className="funding-card">
+                <div className="amount">$140,000</div>
+                <div className="title">Raised</div>
+              </div>
+              <div className="funding-card">
+                <div className="amount">$140,000</div>
+                <div className="title">Funding Goal</div>
+              </div>
+              <div className="funding-card">
+                <div className="amount">4</div>
+                <div className="title">Investors</div>
+              </div>
+              <div className="funding-card">
+                <div className="amount">$3.00</div>
+                <div className="title">Share Price</div>
+              </div>
+              <div className="funding-card">
+                <div className="amount">$140,000</div>
+                <div className="title">Pre-Money Valuation</div>
+              </div>
+              <div className="funding-card">
+                <div className="amount">Preferred Equity</div>
+                <div className="title">Security Type</div>
+              </div>
+            </div>
+            <div className="bar">
+              <div className="progress">
+                <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style={{ width: "80%" }} />
+              </div>
+              <div className="clearfix">
+                <div className="pull-right"><b>80%</b> achieved</div>
+              </div>
+            </div>
+            <button className="btn btn-primary btn-block">SIGNUP TO INVEST</button>
+            <div className="warning">
+              <h4>Purchased securities are not currently tradeable.</h4>
+              <h4 className="instruction">Expect to hold your investment until the company lists on a nation al exchange or is acquired.</h4>
+            </div>
+          </div>
+        </section>
+
+        <hr />
+
+        <section className="row more-info">
+          something
+        </section>
       </div>
     )
   }
