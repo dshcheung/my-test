bdae7dcd9aecdfda3f15780332cd6a2ff397a905
Gram <g@dented.io>
2017-12-13 18:17:32 +0800

    added new style for notifications ... not yet rendering

--- src/components/_pages/my/notifications/index.jsx
+++ src/components/_pages/my/notifications/index.jsx
@@ -1,25 +1,84 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-// import { bindActionCreators } from 'redux'
+import { bindActionCreators } from 'redux'
+import { Link } from 'react-router'
+
+import {
+  getMyNotifications, GET_MY_NOTIFICATIONS,
+  resetMyNotifications
+} from '../../../../actions/my/notifications'
+
+import LoadingSpinner from '../../../shared/loading-spinner'
 
 const mapStateToProps = (state) => {
   return {
-    myNotifications: _.get(state, 'myNotifications', [])
+    myNotifications: _.get(state, 'myNotifications', []),
+    getMyNotificationsInProcess: _.get(state.requestStatus, GET_MY_NOTIFICATIONS)
   }
 }
 
-const mapDispatchToProps = () => {
+const mapDispatchToProps = (dispatch) => {
   return {
+    getMyNotifications: bindActionCreators(getMyNotifications, dispatch),
+    resetMyNotifications: bindActionCreators(resetMyNotifications, dispatch)
   }
 }
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class MyNotificationsIndex extends Component {
+  componentWillMount() {
+    this.props.getMyNotifications()
+  }
+
+  componentWillUnmount() {
+    this.props.resetMyNotifications()
+  }
+
   render() {
+    const { myNotifications, getMyNotificationsInProcess } = this.props
+
     return (
       <div id="page-my-notifications-index" className="container padding-top-20">
         <div className="row">
           <h1 className="page-title margin-bottom-20 margin-top-0">My Notifications</h1>
+        </div>
+        <div className="row">
+          <div className="list-group notifications">
+            {
+              (() => {
+                let component = null
+                if (getMyNotificationsInProcess) {
+                  component = <LoadingSpinner />
+                } else {
+                  const noNotifications = myNotifications.length === 0 && (
+                    <Link to="#" key="no-notifications" className="list-group-item list-group-item-action flex-column align-items-start">
+                      <div className="d-flex w-100 justify-content-between">
+                        <h5 className="mb-1">No Notifications Found</h5>
+                      </div>
+                    </Link>
+                  )
+
+                  const notificationList = myNotifications.map((myNotification, i) => {
+                    const authorProfile = _.get(myNotification, 'author.profile', null)
+                    const userProfile = _.get(myNotification, 'user.profile', null)
+                    return (
+                      <Link key={i} to={``} className="list-group-item list-group-item-action flex-column align-items-start">
+                        <div className="d-flex w-100 justify-content-between">
+                          <h5 className="mb-1">{`${authorProfile.first_name} ${authorProfile.last_name}`}</h5>
+                          <small>{myNotification.created_at}</small>
+                        </div>
+                        <p className="mb-1">{myNotification.message}</p>
+                        <small>{`${userProfile.first_name} ${userProfile.last_name}`}</small>
+                      </Link>
+                    )
+                  })
+
+                  component = [noNotifications, ...notificationList]
+                }
+                return component
+              })()
+            }
+          </div>
         </div>
       </div>
     )
