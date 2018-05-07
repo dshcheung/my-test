d6aa8541a27cf1576f8d0c1142bbaee53d318121
Denis Cheung <d.s.h.cheung@gmail.com>
2018-05-07 12:06:35 +0800

    attachment immovables

--- src/components/forms/my/startup-questionnaires/attachments.jsx
+++ src/components/forms/my/startup-questionnaires/attachments.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { reduxForm, FieldArray } from 'redux-form'
 
 import Validators from '../../../../services/form-validators'
@@ -7,6 +9,25 @@ import Select2Field from '../../../shared/form-elements/select2-field'
 import FileField from '../../../shared/form-elements/file-field'
 import DynamicFieldArray from '../../../shared/form-elements/dynamic-field-array'
 
+import {
+  gImmovable, G_IMMOVABLE_ATTACHMENT_OPTIONS, resetImmovable
+} from '../../../../actions/immovables'
+
+const mapStateToProps = (state) => {
+  return {
+    gImmovableInProcess: _.get(state.requestStatus, G_IMMOVABLE_ATTACHMENT_OPTIONS),
+    attachmentOptions: _.get(state, 'immovables.attachment_options', [])
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    gImmovable: bindActionCreators(gImmovable, dispatch),
+    resetImmovable: bindActionCreators(resetImmovable, dispatch),
+  }
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
 @reduxForm({
   form: "MyStartupQuestionnaireAttachmentsForm",
   validate: (values) => {
@@ -28,10 +49,17 @@ import DynamicFieldArray from '../../../shared/form-elements/dynamic-field-array
 })
 
 export default class MyStartupQuestionnaireAttachmentsForm extends Component {
+  componentWillMount() {
+    this.props.gImmovable({ immovableID: "attachment_options" })
+  }
+
+  componentWillUnmount() {
+    this.props.resetImmovable()
+  }
+
   render() {
     const { handleSubmit, submitInProcess, optClass, initialValues, dMSQAttributes } = this.props
 
-    // TODO: Immovables for opations names
     return (
       <div className={optClass}>
         <form onSubmit={handleSubmit}>
@@ -53,13 +81,11 @@ export default class MyStartupQuestionnaireAttachmentsForm extends Component {
                   key: "title",
                   component: Select2Field,
                   opts: {
-                    options: [
-                      { name: "test1" },
-                      { name: "test2" }
-                    ],
+                    options: this.props.attachmentOptions,
                     valueKey: "name",
                     nameKey: "name",
-                    placeholder: "Title"
+                    placeholder: "Title",
+                    requestInProcess: this.props.gImmovableInProcess
                   }
                 },
                 {
