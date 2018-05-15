66c81dcda7f7e8d2854235941d440790166c863a
Denis Cheung <d.s.h.cheung@gmail.com>
2018-05-15 12:08:14 +0800

    pitch deck slider 2

--- src/components/shared/slider.jsx
+++ src/components/shared/slider.jsx
@@ -14,7 +14,7 @@ export default class SharedSlider extends Component {
 
     this.setSliderHeight = this.setSliderHeight.bind(this)
     this.windowResize = this.windowResize.bind(this)
-    this.exitFullscreen = this.exitFullscreen.bind(this)
+    this.triggerExit = this.triggerExit.bind(this)
     this.makeFullscreen = this.makeFullscreen.bind(this)
   }
 
@@ -23,21 +23,21 @@ export default class SharedSlider extends Component {
   }
 
   componentWillUnmount() {
-    window.removeEventListener('webkitfullscreenchange', this.exitFullscreen)
-    window.removeEventListener('mozfullscreenchange', this.exitFullscreen)
-    window.removeEventListener('fullscreenchange', this.exitFullscreen)
+    window.removeEventListener('webkitfullscreenchange', this.triggerExit)
+    window.removeEventListener('mozfullscreenchange', this.triggerExit)
+    window.removeEventListener('fullscreenchange', this.triggerExit)
     window.removeEventListener('resize', this.windowResize)
   }
 
   onMount() {
     const parentElem = this._reactInternalInstance._renderedComponent._hostNode.parentElement
-    const sliderElem = parentElem.querySelector('.carousel')
+    const sliderElem = parentElem.querySelector(`#${this.props.id}`)
     const itemElems = sliderElem.querySelectorAll("img")
 
     // bind onExit Fullscreen event
-    window.addEventListener('webkitfullscreenchange', this.exitFullscreen)
-    window.addEventListener('mozfullscreenchange', this.exitFullscreen)
-    window.addEventListener('fullscreenchange', this.exitFullscreen)
+    window.addEventListener('webkitfullscreenchange', this.triggerExit)
+    window.addEventListener('mozfullscreenchange', this.triggerExit)
+    window.addEventListener('fullscreenchange', this.triggerExit)
     window.addEventListener('resize', this.windowResize)
     // set initial Slider Height
     this.setSliderHeight(false)
@@ -68,11 +68,21 @@ export default class SharedSlider extends Component {
     this.setSliderHeight(this.state.fullscreenMode)
   }
 
-  exitFullscreen() {
+  triggerExit() {
     if (!document.webkitFullscreenElement &&
       !document.mozFullScreenElement &&
       !document.msFullscreenElement) {
       this.setState({ fullscreenMode: false })
+    }
+  }
+
+  exitFullsreen() {
+    if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen()
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen()
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen()
     }
   }
 
@@ -92,7 +102,7 @@ export default class SharedSlider extends Component {
 
   render() {
     const { id, srcKey, titleKey, data } = this.props
-    const { itemElems, sliderStyle } = this.state
+    const { itemElems, sliderStyle, fullscreenMode } = this.state
 
     const emptyData = data.length === 0
 
@@ -121,7 +131,19 @@ export default class SharedSlider extends Component {
             })
           }
         </Carousel>
-        <button onClick={this.makeFullscreen}>Fullscreen</button>
+        {
+          fullscreenMode ? (
+            <button
+              className="btn btn-sm btn-info"
+              onClick={this.exitFullsreen}
+            ><i className="fas fa-compress" /></button>
+          ) : (
+            <button
+              className="btn btn-sm btn-info"
+              onClick={this.makeFullscreen}
+            ><i className="fas fa-expand" /></button>
+          )
+        }
       </div>
     )
   }
