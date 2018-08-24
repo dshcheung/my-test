48347283cba4d2eca0dacd9adc6318b31f122851
Denis Cheung <d.s.h.cheung@gmail.com>
2018-08-24 16:19:41 +0800

    move hit to the bottom

--- src/components/shared/form-elements/text-area.jsx
+++ src/components/shared/form-elements/text-area.jsx
@@ -17,13 +17,13 @@ export default class TextArea extends Component {
       <div className={`form-group flex-column clearfix ${hasErrorClass}`}>
         { label && <label htmlFor={input.name}>{label} {hasErrorClass && <span className="help-block">{touched ? error.join(", ") : ''}</span>} </label> }
         { !label && <span className="help-block">{touched && hasErrorClass && error.join(", ")}&nbsp;</span>}
-        { hint && <span className="help-block hint">{hint}</span> }
         <textarea
           className="form-control"
           placeholder={placeholder}
           rows="2"
           {...input}
         />
+        { hint && <span className="help-block hint">{hint}</span> }
       </div>
     )
   }
