bcd3eab7d796a69f680efb9e661a5ff2e6b8c986
Gram <g@whub.io>
2018-10-28 18:58:51 +0800

    contact us

--- src/components/_pages/others/company/contact-us.jsx
+++ src/components/_pages/others/company/contact-us.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 import SharedOthersSideTitle from '../../../shared/others/side-title'
 
@@ -9,49 +10,48 @@ export default class ContactUs extends Component {
     return (
       <div>
 
-        <section className="what-we-offer clearfix">
-          <SharedOthersSideTitle title="what we offer" optClass="col-xs-2 col-lg-3 color-primary" />
-
-          <div className="col-xs-8 col-lg-6">
-            <div className="row cards">
-              <div className="col-xs-6 card">
-                <div className="h5 text-uppercase">VENTURE AS AN ASSET CLASS</div>
-                <div>Less than 5% of startups that apply get accepted by AngelHub. Listed startups have to get through our rigorous due diligence process</div>
-              </div>
-
-              <div className="col-xs-6 card">
-                <div className="h5 text-uppercase">BUILD A DIVERSIFIED STARTUP PORTFOLIO</div>
-
-                <div>Invest in startups from around the world across industries and sectors starting at a minimum investment of US$ 10,000</div>
-              </div>
-
-              <div className="col-xs-6 card">
-                <div className="h5 text-uppercase">INVEST IN THE FUTURE</div>
-
-                <div>Startups are driving innovation and changing the future and changing the status quo. Be a part of the digital revolution and empower startups to drive wealth and economic growth</div>
-              </div>
-
-              <div className="col-xs-6 card">
-                <div className="h5 text-uppercase">POWER OF KEY STRATEGIC PARTNER</div>
-
-                <div>We are a team of builders and operators, immersed in the startup eco- system and have built strong partnerships with incubators, accelerators and startup communities across the world</div>
-              </div>
-
-            </div>
-          </div>
-        </section>
-
-        <section className="launch-today clearfix text-center">
-          <div className="col-xs-12">
+        <section className="launch-today clearfix margin-top-20 margin-bottom-20">
+          <SharedOthersSideTitle title="Contact Us" optClass="col-xs-2 col-lg-2 color-primary" />
+          <div className="col-xs-10 col-lg-9">
             <div className="row title">
-              <div className="h4 text-primary text-uppercase">Contact Us</div>
+              <div className="h2 text-primary text-uppercase">Contact us to see how we can help you</div>
             </div>
 
             <div className="row text-left">
               <OthersMailingForm
-                optClass="col-xs-8 col-xs-offset-2 col-lg-6 col-lg-offset-3 dark"
+                optClass="col-xs-12"
                 onSubmit={() => {}}
               />
+            </div>
+          </div>
+        </section>
+
+        <section className="row map no-gutters">
+          <div className="col-xs-12">
+            <iframe
+              title="Our Office"
+              width="100%"
+              height="350"
+              scrolling="no"
+              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAibTn6EJIjozFBpfzE1zlRwNIPtFDjX8E&q=Remex Centre Hong Kong"
+              allowFullScreen="true"
+            />
+          </div>
+        </section>
+
+        <section className="row no-gutters join bg-dark px-50 margin-top-0">
+          <div className="container">
+            <div className="col-xs-12 ">
+              <div className="title h2">Get started on AngelHub.</div>
+              <div className="section-content">
+                <Link to="/auth/signup-investor" className="btn btn-primary btn-lg text-uppercase">
+                  Join As An Investor
+                </Link>
+                &nbsp;
+                <Link to="/auth/signup-startup" className="btn btn-default btn-lg text-uppercase">
+                  Join As A Starutp
+                </Link>
+              </div>
             </div>
           </div>
         </section>
